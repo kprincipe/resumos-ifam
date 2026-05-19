8a7b5d4760d5b285b4bb035c0e633814d4c6767d
kprincipe <kinanprincipe@outlook.com>
2026-05-19 01:10:49 -0400

    atualizacao 18-05-2026

--- ED/atividades/exercicio_lista_dupla.c
+++ ED/atividades/exercicio_lista_dupla.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <assert.h>
 
 #define BUFFER_MAX 255
 
@@ -16,24 +17,123 @@ typedef struct Aluno {
     char nome[BUFFER_MAX];
     float notas[3]; /* valores das notas */
     struct Aluno *pProx; /* ponteiro para o proximo aluno */
-    struct Aluno *pAnt; /* ponteiro para o aluno anterior */
+    struct Aluno *pAnt; /* ponteiro para o aluno anterior*/
 } Aluno;
 
-Aluno inicio, *pAux, *pAnt;
+Aluno *inicio, *pAux, *pAnt;
+
+void remover_aluno(Aluno *aluno);
+void salvar_aluno(Aluno aluno, char *arquivo);
+int carregar_aluno(Aluno *aluno, FILE *f);
+void alunos();
+void cabecalho();
+void menu();
+void diario();
+void exibir();
+void adicionar(Aluno aluno);
+void inserir();
+void remover();
+
+/* remover aluno de arquivo */
+void remover_aluno(Aluno *aluno) {
+    FILE *f = fopen("alunos.csv", "r");
+    FILE *tmp = fopen("tmp", "w");
+
+    Aluno busca = {0};
+    
+    // carrega alunos do arquivo na estrutura estatica temporaria de aluno (busca)
+    while (carregar_aluno(&busca, f) > 0) {
+        // salvar aluno em arquivo temporario apenas se matriculas forem diferentes da que se deseja remover
+        if (aluno->matricula != busca.matricula)
+            salvar_aluno(busca, "tmp");
+    }
+
+    // renomeia arquivo temporario para subscrever os dados de alunos com a lista atualizada
+    rename("tmp", "alunos.csv");
+}
+
+/* funcao de salvar em arquivo */
+void salvar_aluno(Aluno aluno, char *arquivo) {
+    FILE *f = fopen(arquivo, "a");
+    fprintf(f, "%d,%s,%f,%f\n", aluno.matricula, aluno.nome, aluno.notas[0], aluno.notas[1]);
+    fclose(f);
+}
+
+/* funcao de carregar de arquivo */
+int carregar_aluno(Aluno *aluno, FILE *f) {
+    char buffer[BUFFER_MAX];
+    char elemento[BUFFER_MAX];
+
+    // leitura de uma linha do arquivo
+    fgets(buffer, BUFFER_MAX, f);
+
+    if (feof(f)) return -1;
+
+    int i = 0, tamanho_elemento = 0;
+    int qual_elemento = 0;
+    while (buffer[i] != '\0') {
+        // verifica ocorrencia dos caracteres `,` e `\n` na linha
+        if (buffer[i] == ',' || buffer[i] == '\n') {
+            // copia a partir da virgula uma string ate a proxima virgula
+            strncpy(elemento, buffer + (i - tamanho_elemento), tamanho_elemento);
+            // terminador nulo para string
+            elemento[tamanho_elemento] = '\0';
+            
+            // verifica o indice do elemento na linha e armazena com devidas conversoes
+            switch (qual_elemento) {
+                case 0: aluno->matricula = atoi(elemento); break;
+                case 1: strcpy(aluno->nome, elemento); break;
+                case 2: aluno->notas[0] = atof(elemento); break;
+                case 3: aluno->notas[1] = atof(elemento); break;
+                default: printf("erro: formatacao de csv provavelmente incorreta\n");
+            }
+            
+            // reseta o tamanho do elemento para comecar a contagem do tamanho do proximo
+            tamanho_elemento = 0;
+            // incrementa indice de leitura da linha para pular leitura da virgula
+            i++;
+            // incrementa o indice do elemento na linha
+            qual_elemento++;
+            continue;
+        }
+        // incrementa tamanho do elemento atual sendo lido
+        tamanho_elemento++;
+        // incrementa indice de leitura da linha
+        i++;
+    }
+    
+    // processa media aritmetica das notas
+    aluno->notas[2] = (aluno->notas[0] + aluno->notas[1]) / 2;
+    return 1;
+}
+
+/* funcao de alunos */
+void alunos() {
+    FILE *f = fopen("alunos.csv", "r");
+    if (f == NULL) return;
+
+    Aluno aluno = {0};
+    
+    while (carregar_aluno(&aluno, f) > 0) {
+        adicionar(aluno);
+    }
+}
 
 /* funcao de cabecalho */
-void cabecalho(){
+void cabecalho() {
     system("clear");
     printf("programa para gerencia a matricula, o nome e as notas\n");
     printf("de um aluno usando uma lista simplesmente encadeada\n\n");
 }
 
 /* funcao menu */
-void menu(){
+void menu() {
     printf("--------------- menu ----------------\n");
     printf("1................exibir\n");
     printf("2................inserir\n");
     printf("3................remover\n");
+    printf("4................retificar\n");
+    printf("5................relatorio de aluno\n");
     printf("0................sair\n\n");
     printf("digite a opcao: ");
 
@@ -59,7 +159,7 @@ void diario(){
 
 /* funcao exibir */
 void exibir(){
-    pAux = inicio.pProx; /* aponta para o inicio da lista */
+    pAux = inicio; /* aponta para o inicio da lista */
     system("clear");
     printf("--------------- diario ---------------\n");
     printf("\t professor: %s\n", professor);
@@ -67,7 +167,7 @@ void exibir(){
     printf("\t     turma: %s\n", turma);
     printf("--------------------------------------\n\n");
 
-    while(pAux){
+    do {
         printf("\t matricula: %d\n", pAux->matricula);
         printf("\t      nome: %s\n", pAux->nome);
         printf("\t    nota 1: %.2f\n", pAux->notas[0]);
@@ -75,48 +175,62 @@ void exibir(){
         printf("\t     media: %.2f\n\n", pAux->notas[2]);
 
         pAux = pAux->pProx;
-    }
+    } while(pAux->pProx != NULL);
+
     printf("--------------------------------------\n");
     printf("pressione enter para continuar!");
     getchar();
 }
+
+/* adicionar no ao fim da lista */
+void adicionar(Aluno aluno) {
+    pAux = inicio;
+
+    while (pAux->pProx) pAux = pAux->pProx;
+
+    if (pAux->pAnt == NULL) {
+        memcpy(pAux, &aluno, sizeof(Aluno));
+        return;
+    }
+
+    pAux->pProx = malloc(sizeof(Aluno));
+    pAnt = pAux;
+    pAux = pAux->pProx;
+    pAux->pProx = NULL;
+    pAux->pAnt = pAnt;
+    memcpy(pAux, &aluno, sizeof(Aluno));
+}
+
 /* funcao inserir */
 void inserir() {
-    pAux = &inicio; /* aponta para o inicio da lista */
-    while(pAux->pProx) {
-        pAnt = pAux;
-        pAux = pAux->pProx;
-    }
+    Aluno aluno = {0};
 
     do {
         system("clear");
         printf("-------------- cadastro --------------\n");
 
-        pAux->pProx = malloc(sizeof(Aluno));
-        pAux = pAux->pProx;
-        if (pAnt != NULL) pAux->pAnt = pAnt;
-
         char buffer[BUFFER_MAX];
 
         printf("\t    matricula: ");
         fgets(buffer, BUFFER_MAX, stdin);
-        pAux->matricula = atoi(buffer);
+        aluno.matricula = atoi(buffer);
 
         printf("\tnome do aluno: ");
-        fgets(pAux->nome, BUFFER_MAX, stdin);
-        pAux->nome[strlen(pAux->nome) - 1] = '\0';
+        fgets(aluno.nome, BUFFER_MAX, stdin);
+        aluno.nome[strlen(aluno.nome) - 1] = '\0';
 
         printf("\t       nota 1: ");
         fgets(buffer, BUFFER_MAX, stdin);
-        pAux->notas[0] = atoi(buffer);
+        aluno.notas[0] = atof(buffer);
 
         printf("\t       nota 2: ");
         fgets(buffer, BUFFER_MAX, stdin);
-        pAux->notas[1] = atoi(buffer);
+        aluno.notas[1] = atof(buffer);
 
-        pAux->notas[2] = (pAux->notas[0] + pAux->notas[1]) / 2;
-
-        pAux->pProx = NULL;
+        aluno.notas[2] = (aluno.notas[0] + aluno.notas[1]) / 2;
+        
+        adicionar(aluno);
+        salvar_aluno(*pAux, "alunos.csv");
 
         printf("--------------------------------------\n");
 
@@ -125,8 +239,9 @@ void inserir() {
         *resp = toupper(*resp);
     } while (*resp == 'S');
 }
+
 /* funcao remover */
-void remover(){
+void remover() {
     *resp = '0';
 
     system("clear");
@@ -138,7 +253,7 @@ void remover(){
     fgets(buffer, BUFFER_MAX, stdin);
     matTemp = atoi(buffer);
 
-    pAux = &inicio;
+    pAux = inicio;
 
     while (pAux->matricula != matTemp && pAux->pProx != NULL) {
         pAnt = pAux;
@@ -151,10 +266,14 @@ void remover(){
         *resp = toupper(*resp);
 
         if (*resp == 'S') {
-            if (pAux->pProx == NULL && pAux->pAnt == NULL)
-                free(pAux);
-                
-       }
+            pAnt->pProx = pAux->pProx;
+            pAux->pProx = NULL;
+            pAnt = NULL;
+
+            remover_aluno(pAux);
+
+            free(pAux);
+        }
     } else {
         printf("matricula inexistente\n");
         getchar();
@@ -163,17 +282,119 @@ void remover(){
     }
 }
 
+/* funcao de retificacao */
+void retificar() {
+    *resp = '0';
+
+    system("clear");
+
+    printf("-------------- retificar --------------\n");
+    printf("matricula: ");
+
+    char buffer[BUFFER_MAX];
+    fgets(buffer, BUFFER_MAX, stdin);
+    matTemp = atoi(buffer);
+
+    pAux = inicio;
+
+    while (pAux->matricula != matTemp && pAux->pProx != NULL) {
+        pAnt = pAux;
+        pAux = pAux->pProx;
+    }
+
+    if (pAux->matricula == matTemp) {
+        system("clear");
+        printf("retificar cadastro de %s:\n", pAux->nome);
+        printf("1................matricula\n");
+        printf("2................nome\n");
+        printf("3................nota 1\n");
+        printf("4................nota 2\n\n");
+        printf("0................sair\n");
+        printf("digite a opcao: ");
+
+        fgets(buffer, BUFFER_MAX, stdin);
+        
+        switch (atoi(buffer)) {
+            case 1:
+                printf("nova matricula: ");
+                fgets(buffer, BUFFER_MAX, stdin);
+                remover_aluno(pAux);
+                pAux->matricula = atoi(buffer);
+                break;
+            case 0:
+        }
+
+        salvar_aluno(*pAux, "alunos.csv");
+    } else {
+        printf("matricula inexistente\n");
+        getchar();
+        pAnt = NULL;
+        pAux = NULL;
+    }
+}
+
+/* relatorio de aluno */
+void relatorio() {
+    system("clear");
+
+    printf("-------------- relatorio --------------\n");
+    printf("matricula: ");
+
+    char buffer[BUFFER_MAX];
+    fgets(buffer, BUFFER_MAX, stdin);
+    matTemp = atoi(buffer);
+
+    pAux = inicio;
+
+    while (pAux->matricula != matTemp && pAux->pProx != NULL) {
+        pAnt = pAux;
+        pAux = pAux->pProx;
+    }
+
+    if (pAux->matricula == matTemp) {
+        printf("---------------------------------------\n");
+        printf("nome: %s\n\n", pAux->nome);
+        printf("\tmatricula: %d\n", pAux->matricula);
+        printf("\t   nota 1: %.2f\n", pAux->notas[0]);
+        printf("\t   nota 2: %.2f\n", pAux->notas[1]);
+        printf("\t    media: %.2f\n", pAux->notas[2]);
+        printf("---------------------------------------\n");
+        printf("gerar arquivo de relatorio ? sim(s) nao(n): ");
+        fgets(buffer, BUFFER_MAX, stdin);
+        if (toupper(*buffer) == 'S') {
+            sprintf(buffer, "relatorio_%d.txt", pAux->matricula);
+            FILE *f = fopen(buffer, "w");
+            fprintf(f, "---------------------------------------\n");
+            fprintf(f, "relatorio de aluno\n");
+            fprintf(f, "---------------------------------------\n");
+            fprintf(f, "nome: %s\n\n", pAux->nome);
+            fprintf(f, "\tmatricula: %d\n", pAux->matricula);
+            fprintf(f, "\t   nota 1: %.2f\n", pAux->notas[0]);
+            fprintf(f, "\t   nota 2: %.2f\n", pAux->notas[1]);
+            fprintf(f, "\t    media: %.2f\n", pAux->notas[2]);
+            fprintf(f, "---------------------------------------\n");
+            fclose(f);
+            printf("relatorio gerado com sucesso!\n");
+            getchar();
+        }
+    }
+}
+
 /* funcao principal */
 void main (void) {
-    inicio.pProx = NULL; /* lista vazia */
+    inicio = malloc(sizeof(Aluno));
+    inicio->pProx = NULL; /* lista vazia */
+    inicio->pAnt = NULL; /* no anterior ao primeiro da lista */
+
     cabecalho();
     diario();
+    alunos();
 
     do {
         do {
             cabecalho();
             menu();
-            if(opcao < 0 || opcao > 3){
+            if(opcao < 0 || opcao > 5){
                 opcao = -1;
                 printf("opcao invalida!\n");
             }
@@ -190,6 +411,12 @@ void main (void) {
                 break;
             case 3:
                 remover();
+                break;
+            case 4:
+                retificar();
+                break;
+            case 5:
+                relatorio();
                 break;
             default:
                 printf("\nopcao invalida\n");
---
+++ MPN/desenho_de_processos.md
@@ -0,0 +1,13 @@
+# Desenho de Processos
+
+## Fluxo de Processo
+O desenho de processos é a definição formal de objetivos e entregáveis, e a
+organização das atividades e regras necessárias para produzir um resultado
+desejado.
+
+![Fluxo de Processo](imagens/fluxo_de_processos.png)
+
+## Gerenciando o desenho de Processos de Negócio
+Devido ao fato de que na maioria das organizações há poucas abordagens formais
+para desenho de processos, as equipes ficam abertas para definir a abordagem que
+irão utilizar.
